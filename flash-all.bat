bfc5a6d913c5f85e1152697aad16fe8f5e929127
AudioGod <adarsh.mmx@micromaxinfo.com>
2016-09-17 15:14:19 +0530

    YUPHORIA:Add Commands to Flash modem and bootloader
    Signed-off-by: AudioGod <audiogod@sungsonic.co>

--- flash-all.bat
+++ flash-all.bat
@@ -5,6 +5,14 @@
 @fastboot -i 0x2A96 oem unlock 
 @echo ----------------------[ Flashing kernel ]---------------------------
 @fastboot -i 0x2A96 flash boot boot.img
+@echo ---------------[ Flashing modem and bootloader ]--------------------
+@fastboot -i 0x2A96 flash aboot emmc_appsboot.mbn
+@fastboot -i 0x2A96 flash modem NON-HLOS.bin
+@fastboot -i 0x2A96 flash rpm rpm.mbn
+@fastboot -i 0x2A96 flash sbl1 sbl1.mbn
+@fastboot -i 0x2A96 flash tz tz.mbn
+@fastboot -i 0x2A96 flash hyp hyp.mbn
+@fastboot -i 0x2A96 flash splash splash.img
 @echo -------------------[ Flashing cache partition ]---------------------
 @fastboot -i 0x2A96 flash cache cache.img
 @echo -------------------[ Flashing data partition ]----------------------
@@ -13,7 +21,8 @@
 @fastboot -i 0x2A96 flash system system.img
 @echo ----------------------[ Flashing recovery ]-------------------------
 @fastboot -i 0x2A96 flash recovery recovery.img
+@echo ---------------------[ Relocking bootloader ]-----------------------
+@fastboot -i 0x2A96 oem lock
 @echo ---------------------[ Rebooting device ]---------------------------
 @fastboot -i 0x2A96 reboot
 @echo -------------------------[ ALL DONE ]-------------------------------
-
