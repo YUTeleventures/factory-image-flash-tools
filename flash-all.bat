b4bc2c8ea678eaea57f077d28f7472559245741b
AudioGod <adarsh.mmx@micromaxinfo.com>
2016-09-17 15:13:17 +0530

    YUNIQUE: Add Commands to Flash modem and bootloader
    Signed-off-by: AudioGod <audiogod@sungsonic.co>

--- flash-all.bat
+++ flash-all.bat
@@ -1,10 +1,16 @@
-@echo --------------[ Start flashing YU-OPEN-OS ]------------------
+@echo --------------[ Start flashing stock android ROM ]------------------
 @echo ---------------[ Checking for connected devices ]-------------------
 @fastboot devices 
 @echo ----------------[ Checking device info ]------------------
 @fastboot oem device-info
-@echo -------------------[ Unlocking bootloader ]-------------------------
-@fastboot oem unlock
+@echo ---------------[ Flashing modem and bootloader ]--------------------
+@fastboot flash aboot emmc_appsboot.mbn
+@fastboot flash modem NON-HLOS.bin
+@fastboot flash rpm rpm.mbn
+@fastboot flash sbl1 sbl1.mbn
+@fastboot flash tz tz.mbn
+@fastboot flash hyp hyp.mbn
+@fastboot flash splash splash.img
 @echo ----------------------[ Flashing kernel ]---------------------------
 @fastboot flash boot boot.img
 @echo ----------------------[ Flashing recovery ]-------------------------
@@ -15,7 +21,8 @@
 @fastboot flash userdata userdata.img 
 @echo -------------------[ Flashing cache partition ]---------------------
 @fastboot flash cache cache.img
+@echo ----------------[ Checking device info ]------------------
+@fastboot oem device-info
 @echo ---------------------[ Rebooting device ]---------------------------
 @fastboot reboot
 @echo -------------------------[ ALL DONE ]-------------------------------
-
